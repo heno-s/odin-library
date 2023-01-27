5c02d470240cb9b7e3691a69157e9c31ab64035d
janko12 <henrich.sury97@gmail.com>
2023-01-28 00:40:19 +0100

    Display books through js

--- main.js
+++ main.js
@@ -16,3 +16,23 @@ function addBookToLibrary(title, author, pages, didRead) {
     const book = new Book(title, author, pages, didRead);
     myLibrary.push(book);
 }
+
+addBookToLibrary("Harry Potter", "J. K. Rowling", "35", true);
+
+function loadBooks() {
+    myLibrary.forEach(displayBook);
+
+    function displayBook(book) {
+        const bookTemplate = document.querySelector("#book_template");
+        const { title, author, pages, didRead } = book;
+        const bookHtml = bookTemplate.content.firstElementChild;
+        bookHtml.querySelector(".book-title").textContent = title;
+        bookHtml.querySelector(".book-author").textContent = author;
+        bookHtml.querySelector("#pages").textContent = pages;
+
+        const booksContainer = document.querySelector(".books");
+        booksContainer.appendChild(bookHtml);
+    }
+}
+
+loadBooks();
